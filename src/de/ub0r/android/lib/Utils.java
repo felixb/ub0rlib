9bd54aad7bdfc320a7c03f7a1b4660ac12bf87f7
Felix Bechstein <f@ub0r.de>
2010-05-27 18:38:28 +0200

    add md5, base64, DonationHelper

---
+++ src/de/ub0r/android/lib/Utils.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2010 Felix Bechstein
+ * 
+ * This file is part of ub0rlib.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.ub0r.android.lib;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author flx
+ */
+public final class Utils {
+	/** Tag for output. */
+	private static final String TAG = "utls";
+
+	/**
+	 * Default Constructor.
+	 */
+	private Utils() {
+
+	}
+
+	/**
+	 * Calculate MD5 Hash from String.
+	 * 
+	 * @param s
+	 *            input
+	 * @return hash
+	 */
+	public static String md5(final String s) {
+		try {
+			// Create MD5 Hash
+			MessageDigest digest = java.security.MessageDigest
+					.getInstance("MD5");
+			digest.update(s.getBytes());
+			byte[] messageDigest = digest.digest();
+			// Create Hex String
+			StringBuilder hexString = new StringBuilder(32);
+			int b;
+			for (int i = 0; i < messageDigest.length; i++) {
+				b = 0xFF & messageDigest[i];
+				if (b < 0x10) {
+					hexString.append('0' + Integer.toHexString(b));
+				} else {
+					hexString.append(Integer.toHexString(b));
+				}
+			}
+			return hexString.toString();
+		} catch (NoSuchAlgorithmException e) {
+			Log.e(TAG, null, e);
+		}
+		return "";
+	}
+}
