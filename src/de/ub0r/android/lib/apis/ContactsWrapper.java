acc1b0e5ca75e60252bcdbe22a93786cebe3339e
Felix Bechstein <f@ub0r.de>
2010-07-14 07:18:54 +0200

    cleanup *Wrapper.getInstance()

--- src/de/ub0r/android/lib/apis/ContactsWrapper.java
+++ src/de/ub0r/android/lib/apis/ContactsWrapper.java
@@ -59,36 +59,11 @@ public abstract class ContactsWrapper {
 	 */
 	public static final ContactsWrapper getInstance() {
 		if (sInstance == null) {
-
-			String className;
-
-			/**
-			 * Check the version of the SDK we are running on. Choose an
-			 * implementation class designed for that version of the SDK.
-			 * Unfortunately we have to use strings to represent the class
-			 * names. If we used the conventional
-			 * ContactsWrapper5.class.getName() syntax, we would get a
-			 * ClassNotFoundException at runtime on pre-Eclair SDKs. Using the
-			 * above syntax would force Dalvik to load the class and try to
-			 * resolve references to all other classes it uses. Since the
-			 * pre-Eclair does not have those classes, the loading of
-			 * ContactsWrapper5 would fail.
-			 */
 			int sdkVersion = Integer.parseInt(Build.VERSION.SDK);
-			// Cupcake style
 			if (sdkVersion < Build.VERSION_CODES.ECLAIR) {
-				className = "de.ub0r.android.lib.apis.ContactsWrapper3";
+				sInstance = new ContactsWrapper3();
 			} else {
-				className = "de.ub0r.android.lib.apis.ContactsWrapper5";
-			}
-
-			// Find the required class by name and instantiate it.
-			try {
-				Class<? extends ContactsWrapper> clazz = Class.forName(
-						className).asSubclass(ContactsWrapper.class);
-				sInstance = clazz.newInstance();
-			} catch (Exception e) {
-				throw new IllegalStateException(e);
+				sInstance = new ContactsWrapper5();
 			}
 			Log.d(TAG, "getInstance(): " + sInstance.getClass().getName());
 		}
--- src/de/ub0r/android/lib/apis/TelephonyWrapper.java
+++ src/de/ub0r/android/lib/apis/TelephonyWrapper.java
@@ -47,36 +47,11 @@ public abstract class TelephonyWrapper {
 	 */
 	public static final TelephonyWrapper getInstance() {
 		if (sInstance == null) {
-
-			String cn;
-
-			/**
-			 * Check the version of the SDK we are running on. Choose an
-			 * implementation class designed for that version of the SDK.
-			 * Unfortunately we have to use strings to represent the class
-			 * names. If we used the conventional
-			 * TelephonyWrapper4.class.getName() syntax, we would get a
-			 * ClassNotFoundException at runtime on pre-Donut SDKs. Using the
-			 * above syntax would force Dalvik to load the class and try to
-			 * resolve references to all other classes it uses. Since the
-			 * pre-Donut does not have those classes, the loading of
-			 * TelephonyWrapper4 would fail.
-			 */
 			int sdkVersion = Integer.parseInt(Build.VERSION.SDK);
-			// Cupcake style
 			if (sdkVersion < Build.VERSION_CODES.DONUT) {
-				cn = "de.ub0r.android.lib.apis.TelephonyWrapper3";
+				sInstance = new TelephonyWrapper3();
 			} else {
-				cn = "de.ub0r.android.lib.apis.TelephonyWrapper4";
-			}
-
-			// Find the required class by name and instantiate it.
-			try {
-				Class<? extends TelephonyWrapper> clazz = Class.forName(cn)
-						.asSubclass(TelephonyWrapper.class);
-				sInstance = clazz.newInstance();
-			} catch (Exception e) {
-				throw new IllegalStateException(e);
+				sInstance = new TelephonyWrapper4();
 			}
 			Log.d(TAG, "getInstance(): " + sInstance.getClass().getName());
 		}
