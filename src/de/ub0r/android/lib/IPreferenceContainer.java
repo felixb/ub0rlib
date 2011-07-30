4d32f255e6b98f9c37296e6e3618712171a8baed
Felix Bechstein <f@ub0r.de>
2011-07-30 15:32:59 +0200

    add IPreferenceContainer

---
+++ src/de/ub0r/android/lib/IPreferenceContainer.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2010-2011 Felix Bechstein
+ * 
+ * This file is part of ub0rlib.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.ub0r.android.lib;
+
+import android.content.Context;
+import android.preference.Preference;
+
+/**
+ * Switch between PreferenceActivity and PreferenceFragment.
+ * 
+ * @author flx
+ */
+public interface IPreferenceContainer {
+	/**
+	 * Get a {@link Preference}.
+	 * 
+	 * @param key
+	 *            key
+	 * @return {@link Preference}
+	 */
+	Preference findPreference(CharSequence key);
+
+	/**
+	 * Get {@link Context}.
+	 * 
+	 * @return {@link Context}
+	 */
+	Context getContext();
+}
