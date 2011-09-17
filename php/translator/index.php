0fc286d41c9e8cee80ec81cd775e2ea303a5eb2b
Felix Bechstein <f@ub0r.de>
2011-09-17 12:56:02 +0200

    add google analytics

--- php/translator/index.php
+++ php/translator/index.php
@@ -33,6 +33,19 @@ $d->close();
 <head>
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
 <title>Edit Translations</title>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25757356-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
 
