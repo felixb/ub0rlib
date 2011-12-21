428620c5551ec5b51bc32d236f0b696aa0afc08f
Felix Bechstein <f@ub0r.de>
2011-12-21 11:43:16 +0100

    add updatestats.sh

---
+++ shell/updatestats.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+TARGETDIR="/home/flx/dev/translate"
+
+cd "${TARGETDIR}"
+
+for d in */res/ ; do
+  cd "${TARGETDIR}/${d}/.."
+  echo "update: ${PWD}"
+  echo -n "" > translation.stats
+  for f in $(ls -1 res/values/*xml ; ls -1 res/values-??/*xml | sort) ; do
+    fn=$(basename ${f})
+    if [ "${fn}" = "base.xml" -o "${fn}" = "styles.xml" -o "${fn}" = "cwac_touchlist_attrs" ] ; then
+      echo "skip: ${f}"
+    else
+      echo -n ${f}: >> translation.stats
+      grep -Fe '<string' -e'<item' ${f} | grep -v 'notranslation="true"' | wc -l >> translation.stats
+    fi
+  done
+  for l in $(cut -d/ -f2 translation.stats  | sort -u) ; do
+    #echo $l
+    echo -n "res/${l}:" >> translation.stats.sum
+    expr $(grep ${l} translation.stats | cut -d: -f2 | xargs -n9999 | sed -e 's: : + :g') >> translation.stats.sum
+  done
+  cat translation.stats.sum >> translation.stats
+  rm translation.stats.sum
+done
+
