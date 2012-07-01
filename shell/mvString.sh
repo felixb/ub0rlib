221a070f7c8a9cfe062974316b1d0c464a4a0355
Felix Bechstein <f@ub0r.de>
2012-07-01 13:51:58 +0200

    mv strings in res/values/ too

--- shell/mvString.sh
+++ shell/mvString.sh
@@ -4,6 +4,6 @@ p1=$1
 p2=$2
 
 echo "move string: $p1 to $p2"
-for f in $(ls res/values-*/strings.xml) ; do
+for f in $(ls res/values*/strings.xml) ; do
 	sed -i -e "s:name=\"${p1}\":name=\"${p2}\":" $f
 done
