c43336aaee0298697d3d0d93bd3725fda02e3501
Felix Bechstein <f@ub0r.de>
2012-09-03 21:00:57 +0200

    rmStrings.sh

--- shell/rmStrings.sh
+++ shell/rmStrings.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
 for p in $@ ; do
-	echo "remove string: $p"
-	for f in $(ls res/values-*/strings.xml) ; do
-		grep -Fv "name=\"${p}\"" $f > /tmp/callmeter.rmStrings
-		mv /tmp/callmeter.rmStrings $f
-	done
+  echo "remove string: $p"
+  for f in $(grep -lF "$p" res/values*/*.xml) ; do
+    echo "clean file: $f"
+    if (grep -Fq '<string name="'$p'"' $f) ; then
+      sed -e '/string name="'$p'"/d' -i $f
+    elif (grep -Fq '<string-array name="'$p'"' $f) ; then
+      base=$(grep -Fn '<string-array name="'$p'"' $f | cut -d: -f1)
+      end=$(tail -n +$base $f | grep -n '</string-array>' | head -n1 | cut -d: -f1)
+      end=$(($end - 1))
+      sed -e "${base},+${end}d" -i $f
+    fi
+  done
 done
